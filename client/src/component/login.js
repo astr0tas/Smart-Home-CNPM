db15693357db864e3ca3eebab3344fc2a87897a4
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2023-04-01 21:21:05 +0700

    responsive login page

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -8,23 +8,43 @@ const Login = () =>
       https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=
       */
       return (
-            <div className="login">
-                  <div className="container">
+            // <div className="login">
+            //       <div className="container">
+            //             <img className="pic" src="https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=" alt="pic"></img>
+            //             <div className='login-box'>
+            //                   <h1 className='title'>Đăng nhập</h1>
+            //                   <form>
+            //                         <div className="d-flex align-items-center">
+            //                               <AiOutlineUser size={30} className="icons"></AiOutlineUser>
+            //                               <input type="text" name='uname' placeholder="Tên đăng nhập" className='inp'></input>
+            //                         </div>
+            //                         <div className="d-flex align-items-center">
+            //                               <AiOutlineLock size={ 30 } className="icons"></AiOutlineLock>
+            //                               <input type="password" name='password' placeholder="Mật khẩu" className='inp'></input>
+            //                         </div>
+            //                         <input type="submit" className='button' value="Truy cập" />
+            //                   </form>
+            //                   <a href='#' className='link'>Quên mật khẩu?</a>
+            //             </div>
+            //       </div>
+            // </div>
+            <div className="login w-100 h-100 d-flex justify-content-center align-items-center">
+                  <div className="w-75 h-75 login-board d-flex justify-content-center align-items-center">
                         <img className="pic" src="https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=" alt="pic"></img>
-                        <div className='login-box'>
-                              <h1 className='title'>Đăng nhập</h1>
-                              <form>
+                        <div className="d-flex flex-column form">
+                              <h1>Đăng nhập</h1>
+                              <form className="d-flex flex-column">
                                     <div className="d-flex align-items-center">
-                                          <AiOutlineUser size={30} className="icons"></AiOutlineUser>
-                                          <input type="text" name='uname' placeholder="Tên đăng nhập" className='inp'></input>
+                                          <AiOutlineUser className="icons"></AiOutlineUser>
+                                          <input type="text" name='username' placeholder="Tên đăng nhập" className="input"></input>
                                     </div>
                                     <div className="d-flex align-items-center">
-                                          <AiOutlineLock size={ 30 } className="icons"></AiOutlineLock>
-                                          <input type="password" name='password' placeholder="Mật khẩu" className='inp'></input>
+                                          <AiOutlineLock className="icons"></AiOutlineLock>
+                                          <input type="password" name='password' placeholder="Mật khẩu" className="input"></input>
                                     </div>
-                                    <input type="submit" className='button' value="Truy cập" />
+                                    <input type="submit" value="Truy cập" className="submit" />
                               </form>
-                              <a href='#' className='link'>Quên mật khẩu?</a>
+                              <a href='#' className="text-decoration-none forgot">Quên mật khẩu?</a>
                         </div>
                   </div>
             </div>
