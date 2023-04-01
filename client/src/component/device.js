d462e4f27eb1dbe4774cf0a79bda66a73685e423
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2023-04-01 14:50:34 +0700

    updates

--- client/src/component/device.js
+++ client/src/component/device.js
@@ -29,23 +29,44 @@ const Device = () =>
       }
 
       return (
-            <div className="device-page d-flex flex-column justify-content-center">
-                  <div className="device-page-board">
-                        <div className="d-flex justify-content-center choose-device">
-                              <h1>Chọn thiết bị</h1>
+            // <div className="device-page d-flex flex-column justify-content-center">
+            //       <div className="device-page-board">
+            //             <div className="d-flex justify-content-center choose-device">
+            //                   <h1>Chọn thiết bị</h1>
+            //             </div>
+            //             <div className="device-catergory">
+            //                   <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "fans") }>
+            //                         <BsFan className="device_image fans" />
+            //                         <h1 className="fans">Quạt</h1>
+            //                   </a>
+            //                   <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "lights") }>
+            //                         <TiLightbulb className="device_image lights" />
+            //                         <h1 className="lights">Đèn</h1>
+            //                   </a>
+            //                   <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "doors") }>
+            //                         <BsDoorOpen className="device_image doors" />
+            //                         <h1 className="doors">Cửa</h1>
+            //                   </a>
+            //             </div>
+            //       </div>
+            // </div>
+            <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+                  <div className="h-75 w-75 sensor-page-board d-flex flex-column">
+                        <div className="d-flex justify-content-center select-device">
+                              <h1>Chọn cảm biến</h1>
                         </div>
-                        <div className="device-catergory">
+                        <div className="d-flex flex-column flex-md-row w-100 h-75 justify-content-around justify-content-md-around align-items-center m-auto">
                               <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "fans") }>
                                     <BsFan className="device_image fans" />
-                                    <h1 className="fans">Quạt</h1>
+                                    <p className="fans">Quạt</p>
                               </a>
                               <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "lights") }>
                                     <TiLightbulb className="device_image lights" />
-                                    <h1 className="lights">Đèn</h1>
+                                    <p className="lights">Đèn</p>
                               </a>
                               <a href="#" className="d-flex flex-column justify-content-center align-items-center text-decoration-none text-dark devices" onClick={ (e) => handleClick(e, "doors") }>
                                     <BsDoorOpen className="device_image doors" />
-                                    <h1 className="doors">Cửa</h1>
+                                    <p className="doors">Cửa</p>
                               </a>
                         </div>
                   </div>
--- client/src/component/sensor.js
+++ client/src/component/sensor.js
@@ -13,15 +13,15 @@ const Sensor = () =>
             $("#sensor").css("color", "blue");
       })
 
-      // const handleClick = (event, class_name) =>
-      // {
-      //       event.preventDefault();
-      //       $("." + class_name).css("color", "rgb(123, 123, 123)");
-      //       window.setTimeout(() =>
-      //       {
-      //             $("." + class_name).css("color", "black");
-      //       }, 50);
-      // }
+      const handleClick = (event, class_name) =>
+      {
+            event.preventDefault();
+            $("." + class_name).css("color", "rgb(123, 123, 123)");
+            window.setTimeout(() =>
+            {
+                  $("." + class_name).css("color", "black");
+            }, 50);
+      }
 
       return (
             // <div className="sensor-page d-flex flex-column justify-content-center align-items-center">
@@ -55,12 +55,31 @@ const Sensor = () =>
             // </div>
 
             <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
-                  <div className="h-75 w-75 sensor-page-board d-flex flex-column">
-                        <div className="d-flex justify-content-center">
-                              <h1 className="mt-3 mb-2">Chọn cảm biến</h1>
+                  <div className="h-75 w-75 device-page-board d-flex flex-column">
+                        <div className="d-flex justify-content-center select-sensor">
+                              <h1>Chọn cảm biến</h1>
                         </div>
-                        <div>
-
+                        <div className="d-flex flex-column w-100 h-75 justify-content-around justify-content-md-between align-items-center m-auto">
+                              <div className="d-flex flex-column flex-md-row justify-content-around justify-content-md-between align-items-center w-50 m-auto group">
+                                    <a href="#" className="d-flex flex-column justify-content-center align-items-center sensors" onClick={ (e) => handleClick(e, 'thermal') }>
+                                          <BsThermometerSnow className="sensor_image thermal"></BsThermometerSnow>
+                                          <p className="thermal">Nhiệt độ</p>
+                                    </a>
+                                    <a href="#" className="d-flex flex-column justify-content-center align-items-center sensors " onClick={ (e) => handleClick(e, 'wet') }>
+                                          <WiHumidity className="sensor_image wet"></WiHumidity>
+                                          <p className="wet" >Độ ẩm</p>
+                                    </a>
+                              </div>
+                              <div className="d-flex flex-column flex-md-row justify-content-evenly justify-content-md-between align-items-center w-50 m-auto group" >
+                                    <a href="#" className="d-flex flex-column justify-content-center align-items-center sensors" onClick={ (e) => handleClick(e, 'ligh_intensity') }>
+                                          <MdOutlineLightMode className="sensor_image ligh_intensity"></MdOutlineLightMode>
+                                          <p className="ligh_intensity">Ánh sáng</p>
+                                    </a>
+                                    <a href="#" className="d-flex flex-column justify-content-center align-items-center sensors" onClick={ (e) => handleClick(e, 'IR') }>
+                                          <MdOutlineAir className="sensor_image IR"></MdOutlineAir>
+                                          <p className="IR">Hồng ngoại</p>
+                                    </a>
+                              </div>
                         </div>
                   </div>
             </div>
