d71564716ce5b4bc12a4a44d146f08b964dd5ac2
tien0709 <114787726+tien0709@users.noreply.github.com>
2023-04-07 11:36:39 +0700

    Add files via upload

--- gateWay.py
+++ gateWay.py
@@ -1,7 +1,8 @@
 import random
 import time
-import  sys
-from  Adafruit_IO import  MQTTClient
+import sys
+import Adafruit_IO
+from  Adafruit_IO import MQTTClient
 import mysql.connector
 import serial . tools . list_ports
 import sqlite3
@@ -10,16 +11,17 @@ import feedparser
 # Kết nối đến Adafruit IO
 AIO_FEED_ID =  "temp" 
 AIO_USERNAME = "pttien"
-AIO_KEY = "aio_gskO80b9K4dagokSrQwe86iv94dj"
+AIO_KEY = "aio_zkvk195s5V3DEF3Hct2FWuXHGYVt"
+client1 = Adafruit_IO.Client(AIO_USERNAME , AIO_KEY)
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
-def  connected(client):
+def  connected1(client):
     print("Ket noi thanh cong...")
     client.subscribe(AIO_FEED_ID)
 
-def  subscribe(client , userdata , mid , granted_qos):
+def  subscribe1(client , userdata , mid , granted_qos):
     print("Subscribe thanh cong...")
 
-def  disconnected(client):
+def  disconnected1(client):
     print("Ngat ket noi...")
     sys.exit (1)
 # Kết nối đến MySQL
@@ -30,7 +32,7 @@ mydb = mysql.connector.connect(
     database="smart_home"
 )
 
-def  message(client , feed_id , payload):
+def  message1(client , feed_id , payload):
             print("Nhan du lieu: " + payload)
             cursor = mydb.cursor()
             cursor.execute("INSERT INTO cam_bien (MA_CB,TEN,TRANG_THAI,NGUONG_TREN,NGUONG_DUOI,nhietdo) VALUES (%s,%s,%s,%s,%s,%s)", ('HEAT01','metvcl',0,3.1,3.2,payload))
@@ -51,10 +53,10 @@ def  message(client , feed_id , payload):
  #   return commPort
 #ser = serial.Serial( port=getPort(), baudrate=115200)
 
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
+client.on_connect = connected1
+client.on_disconnect = disconnected1
+client.on_message = message1
+client.on_subscribe = subscribe1
 client.connect()
 client.loop_background()
 
@@ -71,8 +73,13 @@ while count :
 
 
    #đưa dữ liệu lên feed
-    value = random . randint (0 , 100)
-    client . publish ("temp", value )
+    temperature_feed = client1.feeds("temp")
+    value = client1.receive(temperature_feed.key)
+    #client . publish ("temp", value )
+    cursor = mydb.cursor()
+    cursor.execute("INSERT INTO cam_bien (MA_CB,TEN,TRANG_THAI,NGUONG_TREN,NGUONG_DUOI,nhietdo) VALUES (%s,%s,%s,%s,%s,%s)", ('HEAT02','aothatday',0,3.1,3.2,value.value))
+    cursor.execute("INSERT INTO nhiet_do (MA_CB) VALUES (%s)", ('HEAT02',))
+    mydb.commit()
     
     # Thêm dữ liệu vào MySQL
     #if len(feed.entries) > 0:
