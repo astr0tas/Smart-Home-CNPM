26cd68ff4789c7472c3a04135e58ea7af00016f2
tien0709 <114787726+tien0709@users.noreply.github.com>
2023-04-20 10:49:28 +0700

    Update get_data.py

--- server/model/adafruit/get_data.py
+++ server/model/adafruit/get_data.py
@@ -82,17 +82,59 @@ def generate_random_string():
     """Generate a random string of given length consisting of uppercase letters and digits"""
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
+def Regenerate_random_string(mydb):
+    # Tạo đối tượng cursor để truy vấn
+    cursor = mydb.cursor()
 
+    # Tạo chuỗi ngẫu nhiên ban đầu
+    random_string = generate_random_string(10)
+
+   # Truy vấn và kiểm tra xem giá trị ngẫu nhiên mới tạo ra có trùng với giá trị trong cột thứ 2 trên bất kỳ hàng nào trong bảng hay không
+    while True:
+        query = "SELECT * FROM du_lieu_cam_bien WHERE MA = %s"
+        cursor.execute(query, (random_string,))
+        row = cursor.fetchone()
+        if row is None:
+          break
+        else:
+          random_string = generate_random_string(10)
+
+    # Khi thoát khỏi vòng lặp, giá trị của random_string đã được tạo ra và không trùng với bất kỳ giá trị nào trong cột thứ 2 của bảng
 
 count = 10
 while count:
     random_string = generate_random_string()
-    # đưa dữ liệu lên feed
-    temperature_feed = client1.feeds("yolo-temp")
-    value = client1.receive(temperature_feed.key)
+    # kiem tra trung lap cua gia tri random, neu trung thi random lai
+    Regenerate_random_string(mydb)
+    # get sensor in feed
+    temp_feed = client1.feeds("yolo-temp")
+    humi_feed = client1.feeds("yolo-humi")
+    light_feed = client1.feeds("yolo-light")
+    move_feed = client1.feeds("yolo-move")
+    
+    # value in sensors
+    value_temp = client1.receive(temp_feed.key)
+    value_humi = client1.receive(humi_feed.key)
+    value_light = client1.receive(light_feed.key)
+    value_move = client1.receive(move_feed.key)
+    
+    # feed status
+    temp_status_feed = client1.feeds("yolo-tem-status")
+    humi_status_feed = client1.feeds("yolo-humi-status")
+    light_status_feed = client1.feeds("yolo-light-status")
+    move_status_feed = client1.feeds("yolo-move-status")
+    
+    # value in feed status
+    value_status_temp = client1.receive(temp_status_feed.key)
+    value_status_humi = client1.receive(humi_status_feed.key)
+    value_status_light = client1.receive(light_status_feed.key)
+    value_status_move = client1.receive(move_status_feed.key)
+    
+    # get_status
     # lấy ngày giờ hiện tại trong máy tính
     dt1 = datetime.datetime.now()
     # client . publish ("temp", value )
+    
     cursor = mydb.cursor()
     # Kiểm tra xem giá trị 'John' đã có trong cột 'name' trong bảng 'users' hay chưa
     # nameTB = 'HEAT01'
@@ -105,8 +147,23 @@ while count:
     # else:
     # cursor.execute("UPDATE cam_bien SET TEN=%s, TRANG_THAI=%s WHERE MA_CB=%s",
     #                ('metvcl', 0, 'HEAT01'))
-    cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",
-                   ('HEAT01', random_string, dt1, value.value))
+    if(value_status_temp):
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HEAT01', random_string, dt1, value_temp.value))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HEAT02', random_string, dt1, value_temp.value-0.5))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HEAT03', random_string, dt1, value_temp.value+0.5))
+    if(value_status_humi):
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HUMID01', random_string, dt1, value_humi.value))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HUMID02', random_string, dt1, value_humi.value-1))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('HUMID03', random_string, dt1, value_humi.value+1))
+    if(value_status_light):
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('LIGHT_INTENSE01', random_string, dt1, value_light.value))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('LIGHT_INTENSE02', random_string, dt1, value_light.value-0.5))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('LIGHT_INTENSE03', random_string, dt1, value_light.value+0.5))
+    if(value_status_move):
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('IR01', random_string, dt1, value_move.value))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('IR02', random_string, dt1, value_move.value-0.5))
+            cursor.execute("INSERT INTO du_lieu_cam_bien (MA_CB,MA, THOI_GIAN, GIA_TRI) VALUES (%s,%s,%s,%s)",('IR03', random_string, dt1, value_move.value+0.5))
+                    
     mydb.commit()
 
     # Thêm dữ liệu vào MySQL
