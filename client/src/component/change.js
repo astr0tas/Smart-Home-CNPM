86a1def709ff0287647d252c9cb041f85e9128d3
hikaric <75846355+hikaric@users.noreply.github.com>
2023-06-07 12:12:32 +0700

    Add files via upload

---
+++ client/src/component/change.js
@@ -0,0 +1,61 @@
+import "../css/login.css"
+import React, { useEffect, useState } from 'react';
+import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
+import axios from 'axios';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+
+const Change = () => {
+    /*
+    https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=
+    */
+    const [wrong, setWrong] = useState(false); 
+    const Navigate = useNavigate()
+    const handleForm = (e) => {
+        e.preventDefault();
+        const username = document.getElementsByName("username")[0];
+        const password = document.getElementsByName("password")[0];
+        const verify = document.getElementsByName("verify")[0];
+        axios.post('http://localhost:5000/change', { username: username.value, password: password.value, verify: verify.value })
+            .then(res => {
+                if (res.data === 1) {
+                    Navigate("/login");
+                    localStorage.setItem('username', username.value);
+                    setWrong(false);
+                }
+                else
+                    setWrong(true);
+            })
+            .catch(err => { console.log(err); })
+    };
+    return (
+        <div className="login w-100 h-100 d-flex justify-content-center align-items-center">
+            <div className="w-75 h-75 login-board d-flex justify-content-center align-items-center">
+                <img className="pic" src="https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=" alt="pic"></img>
+                <div className="d-flex flex-column form">
+                    <h1>Quên mật khẩu</h1>
+                    <form className="d-flex flex-column" onSubmit={handleForm}>
+                        <div className="d-flex align-items-center">
+                            <AiOutlineUser className="icons"></AiOutlineUser>
+                            <input type="text" name='username' placeholder="Tên đăng nhập" className="input"></input>
+                        </div>
+                        <div className="d-flex align-items-center">
+                            <AiOutlineLock className="icons"></AiOutlineLock>
+                            <input type="password" name='password' placeholder="Mật khẩu mới" className="input"></input>
+                        </div>
+                        <div className="d-flex align-items-center">
+                            <AiOutlineLock className="icons"></AiOutlineLock>
+                            <input type="password" name='verify' placeholder="Nhập lại mật khẩu mới" className="input"></input>
+                        </div>
+                        {wrong && <div className="d-flex align-items-center" style={{ color: "red", fontSize: "1rem" }}><AiOutlineCloseCircle />Thông tin nhập chưa chính xác!</div>}
+                        <input type="submit" value="Xác nhận" className="submit" />
+                    </form>
+                    <a href='/Login' className="text-decoration-none forgot">Quay lại</a>
+                </div>
+            </div >
+        </div >
+    );
+}
+
+export default Change;
--- client/src/component/login.js
+++ client/src/component/login.js
@@ -1,39 +1,45 @@
 import "../css/login.css"
+import React, { useEffect, useState } from 'react';
 import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
+import axios from 'axios';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 
-const Login = () =>
-{
+const Login = () => {
       /*
       https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=
       */
+      const [wrong, setWrong] = useState(false);
+      const Navigate = useNavigate()
+      const handleForm = (e) => {
+            e.preventDefault();
+            const username = document.getElementsByName("username")[0];
+            const password = document.getElementsByName("password")[0];
+            axios.post('http://localhost:5000/login', { username: username.value, password: password.value })
+                  .then(res => {
+                        if (res.data === 1) {
+                              Navigate("/admin/sensor");
+                              localStorage.setItem('username', username.value);
+                              setWrong(false);
+                        }
+                        else if (res.data === 2) {
+                              Navigate("/sensor");
+                              localStorage.setItem('username', username.value);
+                              setWrong(false);
+                        }
+                        else
+                              setWrong(true);
+                  })
+                  .catch(err => { console.log(err); })
+      };
       return (
-            // <div className="login">
-            //       <div className="container">
-            //             <img className="pic" src="https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=" alt="pic"></img>
-            //             <div className='login-box'>
-            //                   <h1 className='title'>Đăng nhập</h1>
-            //                   <form>
-            //                         <div className="d-flex align-items-center">
-            //                               <AiOutlineUser size={30} className="icons"></AiOutlineUser>
-            //                               <input type="text" name='uname' placeholder="Tên đăng nhập" className='inp'></input>
-            //                         </div>
-            //                         <div className="d-flex align-items-center">
-            //                               <AiOutlineLock size={ 30 } className="icons"></AiOutlineLock>
-            //                               <input type="password" name='password' placeholder="Mật khẩu" className='inp'></input>
-            //                         </div>
-            //                         <input type="submit" className='button' value="Truy cập" />
-            //                   </form>
-            //                   <a href='#' className='link'>Quên mật khẩu?</a>
-            //             </div>
-            //       </div>
-            // </div>
             <div className="login w-100 h-100 d-flex justify-content-center align-items-center">
                   <div className="w-75 h-75 login-board d-flex justify-content-center align-items-center">
                         <img className="pic" src="https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=" alt="pic"></img>
                         <div className="d-flex flex-column form">
                               <h1>Đăng nhập</h1>
-                              <form className="d-flex flex-column">
+                              <form className="d-flex flex-column" onSubmit={handleForm}>
                                     <div className="d-flex align-items-center">
                                           <AiOutlineUser className="icons"></AiOutlineUser>
                                           <input type="text" name='username' placeholder="Tên đăng nhập" className="input"></input>
@@ -42,12 +48,13 @@ const Login = () =>
                                           <AiOutlineLock className="icons"></AiOutlineLock>
                                           <input type="password" name='password' placeholder="Mật khẩu" className="input"></input>
                                     </div>
+                                    {wrong && <div className="d-flex align-items-center" style={{ color: "red", fontSize: "1rem" }}><AiOutlineCloseCircle />Tài khoản hoặc mật khẩu sai!</div>}
                                     <input type="submit" value="Truy cập" className="submit" />
                               </form>
-                              <a href='#' className="text-decoration-none forgot">Quên mật khẩu?</a>
+                              <a href='/Change' className="text-decoration-none forgot">Quên mật khẩu?</a>
                         </div>
-                  </div>
-            </div>
+                  </div >
+            </div >
       );
 }
 
